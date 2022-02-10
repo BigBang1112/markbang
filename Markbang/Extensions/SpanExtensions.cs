06392c0a3e4a241114dd97af4dce645254049d37
Petr Pivoňka <petrpiv1@gmail.com>
2022-02-10 08:21:02 +0100

    Add SpanSplitEnumerator

--- Markbang/Extensions/SpanExtensions.cs
+++ Markbang/Extensions/SpanExtensions.cs
@@ -2,13 +2,13 @@
 
 internal static class SpanExtensions
 {
-    internal static int TrimStartLength(this in ReadOnlySpan<char> line)
+    internal static int TrimStartLength(this in ReadOnlySpan<char> span)
     {
         var length = 0;
 
-        for (var i = 0; i < line.Length; i++)
+        for (var i = 0; i < span.Length; i++)
         {
-            if (line[i] == ' ')
+            if (span[i] == ' ')
             {
                 length++;
             }
@@ -19,5 +19,15 @@ internal static class SpanExtensions
         }
 
         return length;
+    }
+
+    internal static SpanSplitEnumerator Enumerate(this in ReadOnlySpan<char> span, ReadOnlySpan<char> separator)
+    {
+        return new SpanSplitEnumerator(span, separator);
+    }
+
+    internal static SpanSplitEnumerator Enumerate(this in ReadOnlySpan<char> span, char separator)
+    {
+        return new SpanSplitEnumerator(span, separator);
     }
 }
---
+++ Markbang/SpanSplitEnumerator.cs
@@ -0,0 +1,56 @@
+﻿namespace Markbang;
+
+public ref struct SpanSplitEnumerator
+{
+    private readonly char? separatorChar;
+    private readonly ReadOnlySpan<char> separator;
+
+    private ReadOnlySpan<char> rest;
+    private ReadOnlySpan<char> current;
+    private bool isActive;
+
+    internal SpanSplitEnumerator(ReadOnlySpan<char> span, ReadOnlySpan<char> separator)
+    {
+        rest = span;
+        current = default;
+        isActive = true;
+        separatorChar = null;
+        this.separator = separator;
+    }
+
+    internal SpanSplitEnumerator(ReadOnlySpan<char> span, char separator)
+    {
+        rest = span;
+        current = default;
+        isActive = true;
+        separatorChar = separator;
+        this.separator = default;
+    }
+
+    public SpanSplitEnumerator GetEnumerator() => this;
+    public ReadOnlySpan<char> Current => current;
+
+    public bool MoveNext()
+    {
+        if (!isActive)
+        {
+            return false;
+        }
+
+        var index = separatorChar is null ? rest.IndexOf(separator) : rest.IndexOf(separatorChar.Value);
+
+        if (index >= 0)
+        {
+            current = rest[..index];
+            rest = rest[(index + (separatorChar is null ? separator.Length : 1))..];
+
+            return true;
+        }
+
+        isActive = false;
+        current = rest;
+        rest = default;
+
+        return true;
+    }
+}
