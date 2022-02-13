4479b9341340884331384672235c4266424794b5
Petr Pivoňka <petrpiv1@gmail.com>
2022-02-13 01:42:36 +0100

    Update tests and benchmarks

--- Markbang.Benchmarks/MarkdownBenchmarks.cs
+++ Markbang.Benchmarks/MarkdownBenchmarks.cs
@@ -1,24 +1,43 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
 
 namespace Markbang.Benchmarks;
 
 [MemoryDiagnoser]
+[SimpleJob(RuntimeMoniker.Net50)]
+[SimpleJob(RuntimeMoniker.NetCoreApp31)]
 public class MarkdownBenchmarks
 {
     private readonly string gbxnetMdString = File.ReadAllText("Realistic/GBX.NET.md");
     private readonly Markdown gbxnetMd = Markdown.Parse("Realistic/GBX.NET.md");
+    private readonly string cgamectnchallengeMdString = File.ReadAllText("Realistic/CGameCtnChallenge.md");
+    private readonly Markdown cgamectnchallengeMd = Markdown.Parse("Realistic/CGameCtnChallenge.md");
 
     [Benchmark]
-    public Markdown Parse_GBXNETmd()
+    public Markdown Parse_GBXNET_Md()
     {
         using var reader = new StringReader(gbxnetMdString);
         return Markdown.Parse(reader);
     }
 
     [Benchmark]
-    public void Save_GBXNETmd()
+    public void Save_GBXNET_Md()
     {
         using var writer = new StringWriter();
         gbxnetMd.Save(writer);
+    }
+
+    [Benchmark]
+    public Markdown Parse_CGameCtnChallenge_Md()
+    {
+        using var reader = new StringReader(cgamectnchallengeMdString);
+        return Markdown.Parse(reader);
+    }
+
+    [Benchmark]
+    public void Save_CGameCtnChallenge_Md()
+    {
+        using var writer = new StringWriter();
+        cgamectnchallengeMd.Save(writer);
     }
 }
--- Markbang.Tests/Integration/MarkdownTests.cs
+++ Markbang.Tests/Integration/MarkdownTests.cs
@@ -2,9 +2,36 @@
 
 public class MarkdownTests
 {
+    private const string GBXNET_md = "Realistic/GBX.NET.md";
+    private const string CGameCtnChallenge_md = "Realistic/GBX.NET.md";
+
     [Fact]
-    public void Parse_GBXNETmd_ShouldParse()
+    public void Parse_GBXNET_ShouldParse()
     {
-        var md = Markdown.Parse("Realistic/GBX.NET.md");
+        var md = Markdown.Parse(GBXNET_md);
+    }
+
+    [Fact]
+    public void Parse_CGameCtnChallenge_ShouldParse()
+    {
+        var md = Markdown.Parse(CGameCtnChallenge_md);
+    }
+
+    [Fact]
+    public void Save_GBXNET_ShouldSave()
+    {
+        var md = Markdown.Parse(GBXNET_md);
+
+        using var w = new StringWriter();
+        md.Save(w);
+    }
+
+    [Fact]
+    public void Parse_CGameCtnChallenge_ShouldSave()
+    {
+        var md = Markdown.Parse(CGameCtnChallenge_md);
+
+        using var w = new StringWriter();
+        md.Save(w);
     }
 }
